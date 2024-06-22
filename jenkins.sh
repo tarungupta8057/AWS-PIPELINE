358597574e4286120275d73071b3a480b72c7319
Tarun Kumar Gupta <114097502+tarungupta8057@users.noreply.github.com>
2024-06-22 10:57:04 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # step1 --- To install git, java, and maven.
-yum install git java* maven -y
+yum install git java-17 java-11* maven -y
 yum install wget -y
 # step2 --- To download jenkin repo from redhat and import it.
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 # step3 --- To install more java version for jenkins
-amazon-linux-extras install java-openjdk11 -y # this is for amazon linux
+# amazon-linux-extras install java-openjdk11 -y
 yum install jenkins -y
 # step4 --- To select java version
 update-alternatives --config java
